bb415d765af161c6fe7c9035135d92b2722314fa
YALIZZA ELAYNE TORRES CABALLERO <torres.elayne.10@gmail.com>
2024-10-27 12:17:05 -0500

    Mi primer programa

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Yalizza!");
+
+     var entrada = new Scanner (System.in);
+
+System.out.println("Por favor Ingrese su nombre");
+var entero = entrada.nextLine();
+
+
+entrada.close();
+    }
+ }
+
+
